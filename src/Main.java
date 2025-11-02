2c93dc30ee1304b54fdbfce892d5c50bb2fa40f0
gil444lf <jgil85883@gmail.com>
2025-11-02 15:55:57 -0500

    Add example objects for savings calculation

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,14 @@ public class Main {
         // Aquí cada integrante debe crear 3 objetos según las clases
         // Ejemplo: Cliente c1 = new Cliente("Juan", "juan@gmail.com", 2000000);
         // Dejar vacío para completar en grupo
+
+        // JUAN JOSÉ GIL GUTIÉRREZ
+        // Polimorfismo: Usuario referencia a Cliente
+        Usuario usuarioJuan = new Cliente("Juan José Gil Gutiérrez", "juan.gil@correo.com", 2500000);
+        MetaAhorro metaJuan = new MetaAhorro("Laptop Asus TUF", 2400000, 6);
+        CalculadoraAhorro calculadoraJuan = new CalculadoraAhorro(metaJuan, (Cliente) usuarioJuan);
+
+        System.out.println("Ahorro sugerido para Juan: $" + calculadoraJuan.calcularAhorroSugerido());
+
     }
 }
